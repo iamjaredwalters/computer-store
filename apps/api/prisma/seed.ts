54850bdb0f42fc7f2c1391078b8c729572ab55c5
iamjaredwalters <jared.w.walters@gmail.com>
2024-03-23 10:22:35 -0700

    feat: poor mans check if db has already been seeded

--- apps/api/prisma/seed.ts
+++ apps/api/prisma/seed.ts
@@ -4,6 +4,22 @@ import * as products from './products.json';
 const prisma = new PrismaClient();
 
 async function main() {
+  try {
+    const product = await prisma.product.findFirst({
+      where: {
+        title:
+          'Dell OptiPlex Computer Desktop PC, Intel Core i5 3rd Gen 3.2 GHz, 16GB RAM, 2TB HDD, MTG, 22 Inch LED Monitor, RGB Keyboard and Mouse, WiFi, Windows 10 Pro (Renewed)',
+      },
+    });
+    if (product) {
+      console.log('Products already seeded!');
+      return;
+    }
+  } catch (error) {
+    console.error('Error checking if product exists in the database.');
+    throw error;
+  }
+
   for (const record of products) {
     const { 'striked-price': strikedPrice, ...p } = record;
 
